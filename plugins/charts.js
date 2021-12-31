d0cbff15917b04cc432d5d2bc9ee322730e41d08
Maurice Oegerli <maurice@oeger.li>
2021-12-31 17:13:21 +0100

    delete plugins

--- plugins/charts.js
+++
@@ -1,8 +0,0 @@
-import VueChartkick from '../chartkick';
-import 'chartkick/chart.js';
-
-import { defineNuxtPlugin } from '#app';
-
-export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.vueApp.use(VueChartkick);
-});
